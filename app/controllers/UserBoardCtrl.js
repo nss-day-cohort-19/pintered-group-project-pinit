522c935f86f291473439f59fdfe4ee7316a07969
Jason Smith <jasonlsmith52@gmail.com>
2017-06-06 10:42:45 -0500

    makin boards

--- app/controllers/UserBoardCtrl.js
+++ app/controllers/UserBoardCtrl.js
@@ -1,5 +1,28 @@
 "use strict";
 
-app.controller("UserBoardCtrl", function($scope, DataFactory){
+app.controller("UserBoardCtrl", function($scope, DataFactory, AuthFactory){
+    let user = AuthFactory.getUser();
 
+    $scope.getBoards = () => {
+        DataFactory.getBoards(user)
+        .then( (boards) => {
+            $scope.userBoards = boards.data;
+            // for(x in $scope.boards){
+            //     DataFactory.getPins(x)
+            //     .then( (pins) => {
+            //         $scope.boards.x = pins.data;
+            //     });
+            // }
+        });
+    };
+
+    $scope.removeBoard = (boardId) => {
+        DataFactory.deleteBoard(user, boardId)
+        .then( (response) => {
+            $scope.getBoards(user);
+        });
+    };
+
+    $scope.getBoards();
 });
+
---
+++ app/factories/dataFactory.js
@@ -0,0 +1,7 @@
+"use strict";
+
+app.factory("DataFactory", function($q, $http, fbcreds) {
+
+    
+
+};
