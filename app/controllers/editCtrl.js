f52fd1773693c3d1c3bbd07edba0f5f1f0651706
Aarti Jaisinghani <jaisina@Aartis-MacBook-Air.local>
2017-06-06 13:09:36 -0500

    comments

--- app/controllers/editCtrl.js
+++ app/controllers/editCtrl.js
@@ -1,1 +1,30 @@
 "use strict";
+
+app.controller('editCtrl', function($scope, $routeParams, DataFactory, $location) {
+  
+  $scope.pin = {
+  	uid:"",
+    url: "",
+    name: "",
+    description: "",
+    user_text: "",
+    board_id:"",
+    tags:""
+  };
+
+	DataFactory.getPin($routeParams.pinId)
+	.then( (stuff) => {
+		$scope.pin = stuff;
+		$scope.pin.id = $routeParams.pinId;
+	});
+
+  $scope.submitPin = function() {
+    // stuff goes here
+    DataFactory.editPin($routeParams.pinId, $scope.pin)
+    .then( (response) => {
+    	$location.path("/");
+    });
+    console.log("pin", $scope.pin);
+    console.log("You clicked the edit pin button!");
+  };
+});
