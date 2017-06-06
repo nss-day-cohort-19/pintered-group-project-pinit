7a977af9cae0d1b3820d427bcc8c47d6d896c7f2
Ryan McCarty <Ryan McCarty>
2017-06-06 09:39:46 -0500

    put the routes in, authFac, and other stuff

---
+++ app/app.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const app= angular.module("PinItApp", ["ngRoute"]);
+
+app.run(($location, fbcreds)=>{
+   let cred= fbcreds;
+   let authConfig={
+    apiKey: cred.apiKey,
+    authDomain: cred.authDomain,
+    databaseURL: cred.databaseUrl
+   };
+});
+
+
+app.config(($routProvider)=>{
+    $routProvider
+    .when("/",{
+        templateUrl: "partials/allPins.html",
+        controller: "AllPin"
+    })
+    .when("/login", {
+        templateUrl: "partials/login.html",
+        controller: "AuthCtrl"
+    })
+    .when("/logout",{
+        templateUrl: "partials/login.html",
+        controller: "AuthCtrl"
+    })
+    .when("/AllPins",{
+        templateUrl: "partials/allPins.html",
+        controller: "AllPin"
+    })
+    .when("/newPin",{
+        templateUrl: "partials/pinForm.html",
+        controller: "PinFormCtrl"
+    })
+    .when("/newBoard",{
+        templateUrl: "partials/boardForm.html",
+        controller: "BoardFormCtrl"
+    })
+    .when("/boards",{
+        templateUrl: "partials/userBoards.html",
+        controller: "UserBoardCtrl"
+    })
+    .when("/boards/:boardId/edit",{
+        templateUrl: "partials/boardForm",
+        controller: "editCtrl"
+    })
+    .when("/boards/:boardId/:pinId",{
+        templateUrl: "partials/boardDetail.html",
+        controller: "BoardDetailCtrl"
+    })
+    .when("/boards/:boardId/:pinId/edit",{
+        templateUrl: "partials/pinForm.html",
+        controller:  "editCtrl"
+    })
+    .otherwise("/");
+});
