8b66382b431647c30d1eb8a8e938b5b30d46365b
Aarti Jaisinghani <jaisina@Aartis-MacBook-Air.local>
2017-06-06 09:18:43 -0500

    named the controllers

---
+++ app/controllers/BoardDetailCtrl.js
@@ -0,0 +1,5 @@
+"use strict";
+
+app.controller("BoardDetailCtrl", function($scope, DataFactory){
+
+});
---
+++ app/controllers/BoardFormCtrl.js
@@ -0,0 +1,5 @@
+"use strict";
+
+app.controller("BoardFormCtrl", function($scope, DataFactory){
+
+});
