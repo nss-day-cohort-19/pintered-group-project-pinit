1d1c9aa3815bc155b590f4ee55967a475f805799
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-06-06 14:40:59 -0500

    Adding pins to boards puts board_id onto pin data in firebase

--- app/factories/dataFactory.js
+++ app/factories/dataFactory.js
@@ -13,10 +13,10 @@ app.factory("DataFactory", function($q,$http,fbcreds){
         });
     };
 
-    const getBoards = (user) => {
+    const getBoards = () => {
         let boards = [];
         return $q((resolve, reject) => {
-            $http.get(`${fbcreds.databaseURL}/boards.json?orderBy="uid"&equalTo="${user}`)
+            $http.get(`${fbcreds.databaseURL}/boards.json`)
             .then((boardsObj) => {
                 let boardsCollection = boardsObj.data;
                 Object.keys(boardsCollection).forEach((key) => {
@@ -77,7 +77,7 @@ app.factory("DataFactory", function($q,$http,fbcreds){
         // console.log("itemObj", itemObj);
         let itemsArray = [];
         for (let object in itemObj.data){
-          itemObj.data[object].pinID = object;
+          // itemObj.data[object].pinID = object;
           // console.log(itemObj.data[object]);
           itemsArray.push(itemObj.data[object]);
         }
