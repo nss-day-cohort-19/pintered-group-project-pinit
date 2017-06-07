dc6f7e12e8bf0be922c27b76c60abc62bd3e807a
Aarti Jaisinghani <jaisina@Aartis-MacBook-Air.local>
2017-06-07 13:13:13 -0500

    delete pin yayy

--- app/controllers/BoardDetailCtrl.js
+++ app/controllers/BoardDetailCtrl.js
@@ -1,14 +1,34 @@
 "use strict";
 
-app.controller("BoardDetailCtrl", function($scope, DataFactory){
+app.controller("BoardDetailCtrl", function($scope, DataFactory, $routeParams){
 
 	// console.log("$scope", $scope);
 
-	DataFactory.getBoardPins( "-Kly51h5kVTjUMoMLJNX" )
-	.then( (object) => {
-		$scope.pins = object;
-		$scope.boardName = object[0].boards;
-		console.log("$scope.pins", $scope.pins);
-	});
+	$scope.getBoardPins = () => {
+		DataFactory.getBoardPins($routeParams.boardId)
+		.then( (object) => {
+			$scope.pins = object;
+			$scope.boardName = object[0].boards;
+			console.log("$scope.pins", $scope.pins);
+		});
+    };
+
+	$scope.removePin = function (pinID) {
+    // remove a task
+    console.log("pinID", pinID);
+    DataFactory.removePin( pinID )
+    .then( () => {
+      $scope.getBoardPins($routeParams.boardId);
+    });
+  };
+
+  $scope.removeBoard = () => {
+        DataFactory.deleteBoard($routeParams.boardId)
+        .then( (response) => {
+            $scope.getBoards();
+        });
+    };
+
+  $scope.getBoardPins();
 
 });
--- app/controllers/PinFormCtrl.js
+++ app/controllers/PinFormCtrl.js
@@ -32,7 +32,7 @@ console.log("routeParams.pinid is", $routeParams.pinId);
     DataFactory.makePin($scope.pin)
     .then(()=>{
       console.log("$scope.pin", $scope.pin);
-      $window.location.url= "#!/allPins"
+      $window.location.url= "#!/allPins";
     })
     .then( (data)=>{
       $location.path("/allPins");
